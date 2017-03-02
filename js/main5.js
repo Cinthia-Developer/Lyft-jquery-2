8776fccb36eab522fafe5eb89f7eb482ac4e7985
Cinthia <cinthiamashiel474@gmail.com>
2017-03-01 21:55:14 -0500

    Primera pantalla completada

--- js/main5.js
+++ js/main5.js
@@ -2,7 +2,8 @@ function initMap() {
     var myLatLng = {lat: -16.457148 , lng: -71.530759};
     var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
-    center: myLatLng
+    center: myLatLng,
+    disableDefaultUI: true
   });
     var myLatLng = {lat: -16.457148 , lng: -71.530759};
     var marker = new google.maps.Marker({
@@ -10,14 +11,14 @@ function initMap() {
     map: map,
     title: 'Aqui estoy',
   });
-    var myLatLng1 = {lat: -16.455648 , lng: -71.530589};
+    var myLatLng1 = {lat: -16.455648 , lng: -71.535870};
     var marker = new google.maps.Marker({
     position: myLatLng1,
     map: map,
     title: 'Lucas',
     icon: "images/carro1.jpg"
   });
-    var myLatLng2 = {lat: -16.455348 , lng: -71.532789};
+    var myLatLng2 = {lat: -16.459128, lng: -71.525970};
     var marker = new google.maps.Marker({
     position: myLatLng2,
     map: map,
@@ -38,21 +39,21 @@ function initMap() {
     title: 'Steve',
     icon: "images/carro1.jpg"
   });
-    var myLatLng5 = {lat: -16.453228 , lng: 71.52702};
+    var myLatLng5 = {lat: -16.453228, lng: 71.52702};
     var marker = new google.maps.Marker({
     position: myLatLng5,
     map: map,
     title: 'Angui',
     icon: "images/carro2.jpg"
   });
-    var myLatLng6 = {lat: -16.45115 , lng:  -71.529209};
+    var myLatLng6 = {lat: -16.459128, lng: -71.529870};
     var marker = new google.maps.Marker({
     position: myLatLng6,
     map: map,
     title: 'Maycol',
     icon: "images/carro2.jpg"
   });
-    var myLatLng7 = {lat: -16.451284, lng: -71.531966};
+    var myLatLng7 = {lat: -16.455348 , lng: -71.532789};
     var marker = new google.maps.Marker({
     position: myLatLng7,
     map: map,
